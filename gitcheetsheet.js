dfcb1190f9afc43200d08d624141a302a0cb2278
Muhammad Owais <muhammadowaiskhan616@gmail.com>
2023-08-14 21:57:35 +0500

    main merge conflict

---
+++ gitcheetsheet.js
@@ -0,0 +1,11 @@
+// git show -u => show you the commited list
+// git restore filename => it will remove the unstate data
+// git reset filename => it will unstage the data i.e opposite of git add
+// git commint --amend => it updated the latest commit
+// git revert Head => it reverts the recently commited
+// commit (means it will undo all the recent commit data &
+//     update the commit by telling why you did this)
+// git branch => list git branches
+// git checkout branch-name => switch to the branch
+// git checkout -b branch-name => create a new branch & switch to it
+// git branch -d branch-name => delete the branch if it is marged to the original branch
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 console.log("its first");
 console.log("its 1st commit");
 console.log("text of branch one");
+console.log("the main branch conflict");
