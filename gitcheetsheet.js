332089b055f2bd8305d6df64cb8c36b47a972071
Muhammad Owais <muhammadowaiskhan616@gmail.com>
2023-08-15 17:11:38 +0500

    adding readme

--- gitcheetsheet.js
+++ gitcheetsheet.js
@@ -1,3 +1,4 @@
+// Only Git
 // git show -u => show you the commited list
 // git restore filename => it will remove the unstate data
 // git reset filename => it will unstage the data i.e opposite of git add
@@ -9,3 +10,15 @@
 // git checkout branch-name => switch to the branch
 // git checkout -b branch-name => create a new branch & switch to it
 // git branch -d branch-name => delete the branch if it is marged to the original branch
+// git branch -D <name> Forcibly deletes the branch
+// git merge <branch>  Merge joins branches together
+// git merge --abort  to abort the mege conflict
+// git log --graph --oneline => This shows a summarized view of the commit history for a repo
+
+// Git & Github
+// git pull => will pull the updated changes from remote repository & merge it into local
+// git fetch => will fetch  the updated changes from remote repository but don't merge it,
+//    you  will have to merge it manually
+
+// git remote update => fetches the most up to date objects
+// git branch -r => list remote branches
